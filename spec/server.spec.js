114997e1c63d58841fad58a6056f77885fe23cd1
sdepold <sascha@depold.com>
2012-03-15 08:30:04 +0100

    improved server + more tests

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -5,44 +5,73 @@ if(typeof require === 'function') {
 
 buster.spec.expose()
 
+var initServerEnvironment = function() {
+  var self = this
+
+  this.socket = {
+    on: function(){},
+    bind: function(){},
+  }
+
+  this.socketLib = {
+    createSocket: function() { return self.socket }
+  }
+
+  this.socketLibMock = this.mock(this.socketLib)
+  this.socketMock    = this.mock(this.socket)
+  this.server        = new Server({ socketLib: this.socketLib })
+}
+
 describe('Server', function() {
   describe('init', function() {
     before(function() {
-      this.serverLib = {
+      this.socketLib = {
         createSocket: function() {}
       }
-      this.serverLibMock = this.mock(this.serverLib)
+      this.socketLibMock = this.mock(this.socketLib)
     })
 
     it('stores the passed port', function() {
-      var server = new Server({ port: 2222, serverLib: this.serverLib })
+      var server = new Server({ port: 2222, socketLib: this.socketLib })
       expect(server.options.port).toEqual(2222)
     })
 
     it('uses a default port if it is not passed', function() {
-      var server = new Server({ serverLib: this.serverLib })
+      var server = new Server({ socketLib: this.socketLib })
       expect(server.options.port).toEqual(2323)
     })
   })
 
-  // describe('start', function() {
-  //   before(function() {
-  //     this.server = new Server()
-  //   })
+  describe('start', function() {
+    before(function() {
+      initServerEnvironment.call(this)
+    })
 
-  //   it('starts a server', function() {
-  //     var mock = this.mock(this.server.dgram)
+    it('binds a socket', function() {
+      this.socketMock.expects('on').withArgs('listening').once()
+      this.socketMock.expects('on').withArgs('message').once()
+      this.socketMock.expects('bind').withArgs(2323).once()
 
-  //     mock.expects('on').withArgs('listening').once()
-  //     mock.expects('on').withArgs('message').once()
-  //     mock.expects('bind').withArgs(2323).once()
+      this.server.start()
 
-  //     this.server.start()
+      this.socketMock.verify()
+      assert(true)
+    })
+  })
 
-  //     mock.verify()
-  //     assert(true)
-  //     mock.restore()
-  //     this.server.dgram.close()
-  //   })
-  // })
+  describe('handleRequest', function() {
+    before(function() {
+      initServerEnvironment.call(this)
+      this.serverMock = this.mock(this.server)
+    })
+
+    it("checks for expired buckets before requesting them", function() {
+      this.serverMock.expects('checkForExpiredBuckets')
+      this.serverMock.expects('requestBucket')
+      this.server.handleRequest('foo;bar')
+      this.serverMock.verify()
+
+      assert(true)
+    })
+  })
 })
--- src/server.js
+++ src/server.js
@@ -4,31 +4,31 @@ const BucketManager = require("./bucket-manager")
 var Server = module.exports = function(options) {
   this.options = _.extend({
     port: 2323,
-    serverLib: require('dgram')
+    socketLib: require('dgram')
   }, options || {})
-  this.server = this.options.serverLib.createSocket("udp4")
+  this.socket = this.options.socketLib.createSocket("udp4")
   this.bucketManager = new BucketManager()
 }
 
 Server.prototype.start = function() {
   var self = this
 
-  this.server.on("message", function(msg, rinfo) {
+  this.socket.on("message", function(msg, rinfo) {
     console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port)
     self.handleRequest(msg.toString())
   })
 
-  this.server.on("listening", function() {
+  this.socket.on("listening", function() {
     var address = this.address()
     console.log("server listening " + address.address + ":" + address.port)
   })
 
-  this.server.bind(this.options.port);
+  this.socket.bind(this.options.port);
 }
 
 Server.prototype.handleRequest = function(message) {
-  this.requestBucket(message)
   this.checkForExpiredBuckets()
+  this.requestBucket(message)
 }
 
 Server.prototype.requestBucket = function(message) {
@@ -47,8 +47,7 @@ Server.prototype.checkForExpiredBuckets = function() {
   var expired = this.bucketManager.getExpired()
 
   if(expired.length > 0) {
-    console.log('The following buckets are expired: ' + expired.map(function(bucket) {
-      return bucket.identifier
-    }).join(', '))
+    var expiredBucketNames = expired.map(function(bucket) { return bucket.identifier }).join(', ')
+    console.log('The following buckets are expired: ' + expiredBucketNames)
   }
 }
