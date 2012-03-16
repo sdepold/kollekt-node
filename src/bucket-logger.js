fae79a41f4bdf3173e231d8e30b458cbe0b47232
sdepold <sascha@depold.com>
2012-03-16 21:59:37 +0100

    hold stream until new one is needed

--- src/bucket-logger.js
+++ src/bucket-logger.js
@@ -2,12 +2,30 @@ const _    = require("underscore")
     , path = require("path")
     , fs   = require("fs")
 
-var BucketLogger = module.exports = function() {}
+var BucketLogger = module.exports = function() {
+  this.streams = {}
+}
 
 BucketLogger.prototype.log = function(bucket) {
-  var stream = fs.createWriteStream(this.getFilePath(), { flags: 'a' })
-  stream.write(bucket.toString() + "\n")
-  stream.end()
+  var filePath = this.getFilePath()
+    , self     = this
+
+  if(!this.streams[filePath]) {
+    self.streams[filePath] = fs.createWriteStream(filePath, { flags: 'a' })
+    self.closeOldStreams
+  }
+
+  this.streams[filePath].write(bucket.toString() + "\n")
+}
+
+BucketLogger.prototype.closeOldStreams = function() {
+  var filePath = this.getFilePath()
+
+  _.each(this.streams, function(stream, key) {
+    if(key != filePath) {
+      stream.close()
+    }
+  })
 }
 
 BucketLogger.prototype.getFilePath = function() {
