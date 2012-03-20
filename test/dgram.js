532b93d50e94e5ba50ea550c22417be68b7a3e13
sdepold <sascha@depold.com>
2012-03-20 09:08:57 +0100

    better debugging

--- test/dgram.js
+++ test/dgram.js
@@ -1,8 +1,25 @@
-var dgram = require('dgram')
-  ;
-
+var dgram    = require('dgram')
+  , bytes    = 0
+  , mcount   = 0
+  , date     = null
+  , debugged = {}
 
 sock = dgram.createSocket("udp4", function (msg, rinfo) {
-});
+  date   = new Date
+  bytes  += msg.length
+  mcount += 1
 
-sock.bind(8000, '0.0.0.0');
+  if((+date % 1000) == 0) {
+    var dateWithoutMS = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds(), 0)
+
+    if(!debugged[dateWithoutMS]) {
+      debugged[dateWithoutMS] = true
+
+      console.log("Mb/s:", (bytes * 8) / 1024 / 1024, ", m/s: ", mcount)
+      bytes = 0
+      mcount = 0
+    }
+  }
+})
+
+sock.bind(2323, '0.0.0.0')
